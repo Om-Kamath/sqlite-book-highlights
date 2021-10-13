01f32a4a588f151d35a4caf92029572503290084
Rushabh Javeri <javeri.rushabh45@gmail.com>
2021-10-13 20:34:47 +0530

    formatting validate.py per my preference

--- validate.py
+++ validate.py
@@ -1,4 +1,6 @@
 import re
+
+
 def validatePassword(email):
     regex = re.compile('^[a-zA-Z_.0-9]+[@]\w+[.]{1,1}(com|edu|in|us|edu\.in|org)$')
     return bool(re.match(regex,email))
@@ -6,7 +8,10 @@ def validatePassword(email):
 
 def validatePassword(password):
     # ?= makes sure that the code in front matches the previous characters
-    #The conditions are: minimum 8 characters, atleast one upperace and one lowercase character, atleast one special character and atleast one digit
+    # The conditions are:
+      # Minimum 8 characters
+      # At least one upperace and one lowercase character
+      # At least one special character
+      # At least one digit
     regex = re.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
     return bool(re.match(regex,password))
-
